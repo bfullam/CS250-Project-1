26a42fb9d8fb7461f844c27db9a520384e534fed
Bryan Fullam <bryanfullam@gmail.com>
2015-11-01 20:05:04 -0800

    added D files to project
    Add to your project the following files:
     StudentList.h
     StudentList.cpp
     student_data.txt
     InputHandler.h
     Main.cpp (this will replace your old Main.cpp file)

--- Project files/Project/Main.cpp
+++ Project files/Project/Main.cpp
@@ -1,97 +1,123 @@
 /*
 	(name header)
 */
-#include "Student.h"
-#include "Course.h"
+
+#include "InputHandler.h"
 
 #include <iostream>
-#include <vector>
 #include <string>
 #include <iomanip>
 
 using namespace std;
 
+void displayMenu();
+void processChoice(const StudentList& studentList, double tuitionRate);
+
 int main()
-{	
-	Course course;
+{
+	StudentList studentList;
+	double tuitionRate;
 
-	cout << fixed << showpoint << setprecision(2);
-
-	Student peter;
-	vector<Course> peterCourses;
-	course.setCourseInfo("ComputeSci", "CSC150", 'A', 4);
-	peterCourses.push_back(course);
-	course.setCourseInfo("Chemistry", "CHM348", 'A', 4);
-	peterCourses.push_back(course);
-	course.setCourseInfo("English", "ENG101", 'B', 3);
-	peterCourses.push_back(course);
-	course.setCourseInfo("Biology", "BIO234", 'A', 4);
-	peterCourses.push_back(course);
-	peter.setStudentInfo("Peter", "Parker", 654321, true, peterCourses);
-
-	cout << "Student ID: " << peter.getID() << endl;
-	cout << "Number of courses enrolled: " << peter.getNumberOfCourses() << endl;
-	cout << "Total credits: " << peter.getCreditsEnrolled() << endl;
-	if (peter.isTuitionPaid()) 
-		cout << "Tuition was paid." << endl;
-	else 
-		cout << "Tuition was not paid." << endl;
-	if (peter.isEnrolledInCourse("CSC150"))
-		cout << "Peter is enrolled in CSC150" << endl;
-	else
-		cout << "Peter is not enrolled in CSC150" << endl;
-	if (peter.isEnrolledInCourse("BIO111"))
-		cout << "Peter is enrolled in BIO111" << endl;
-	else
-		cout << "Peter is not enrolled in BIO111" << endl;
-	cout << "Total GPA: " << peter.getGpa() << endl;
-	cout << "Total amount paid: $" << peter.billingAmount(50.00) << endl;
-	cout << "Change student's name..." << endl;
-	peter.setName("Pete", "Parker");
-	cout << "\nPrint student's new name..." << endl;
-	peter.printName();
-	cout << "\nPrint all information: " << endl;
-	peter.printStudentInfo();
-	cout << "\nPrint grade report: " << endl;
-	peter.printStudentInfo(50.00);
-
-
-	Student lex;
-	vector<Course> lexCourses;
-	course.setCourseInfo("ComputeSci", "CSC201", 'A', 4);
-	lexCourses.push_back(course);
-	course.setCourseInfo("History", "HIS101", 'A', 3);
-	lexCourses.push_back(course);
-	course.setCourseInfo("Biology", "BIO234", 'A', 4);
-	lexCourses.push_back(course);
-	lex.setStudentInfo("Lex", "Luthor", 345678, false, lexCourses);
-
-	cout << "Student ID: " << lex.getID() << endl;
-	cout << "Number of courses enrolled: " << lex.getNumberOfCourses() << endl;
-	cout << "Total credits: " << lex.getCreditsEnrolled() << endl;
-	if (lex.isTuitionPaid())
-		cout << "Tuition was paid." << endl;
-	else
-		cout << "Tuition was not paid." << endl;
-	if (lex.isEnrolledInCourse("CSC150"))
-		cout << "Lex is enrolled in CSC150" << endl;
-	else
-		cout << "Lex is not enrolled in CSC150" << endl;
-	if (lex.isEnrolledInCourse("HIS101"))
-		cout << "Lex is enrolled in HIS101" << endl;
-	else
-		cout << "Lex is not enrolled in HIS101" << endl;
-	cout << "Total GPA: " << lex.getGpa() << endl;
-	cout << "Total amount due: $" << lex.billingAmount(50.00) << endl;
-	cout << "\nPrint all information: " << endl;
-	lex.printStudentInfo();
-	cout << "\nPrint grade report: " << endl;
-	lex.printStudentInfo(50.00);
-	
+	readStudentData(studentList, tuitionRate);
+	displayMenu();
+	processChoice(studentList, tuitionRate);
 
 	cout << endl;
 	system("Pause");
 	return 0;
 }
 
+void displayMenu()
+{
+	cout << "\n*** MAIN MENU ***\n";
+	cout << "\nSelect one of the following:\n\n";
+	cout << "    1: Print all students" << endl;
+	cout << "    2: Print student information" << endl;
+	cout << "    3: Search student by last name" << endl;
+	cout << "    4: Print students by course" << endl;
+	cout << "    5: Print students on hold" << endl;
+	cout << "    6: To exit" << endl;
 
+}
+
+void processChoice(const StudentList& studentList, double tuitionRate)
+{
+	int choice, id;
+	string courseNumber, lastName;
+
+	cout << "\nEnter your choice: ";
+	cin >> choice; 
+	cout << endl;
+
+	cout << fixed << showpoint << setprecision(2); 
+
+	while (choice != 6)
+	{
+		string fName, lName;
+		int ssn = 0;
+		double donation = 0.0;
+
+		switch(choice)
+		{
+			
+			// Print all students
+		case 1: 
+			studentList.printAllStudents(tuitionRate);
+
+			break;
+
+			// Print student information
+		case 2: 
+			cout << "Please enter student's ID: ";			
+			cin >> id;
+			cout << endl;
+			
+			studentList.printStudentByID(id, tuitionRate);
+			cout << endl;
+
+			break;
+
+			// Search student by last name
+		case 3: 
+			cout << "Please enter the student's last name: ";			
+			cin >> lastName;
+			cout << endl;
+
+			studentList.printStudentsByName(lastName);
+			cout << endl; 
+				
+			break;
+
+			// Print students by course 
+		case 4:
+			cout << "Please enter the course number: ";			
+			cin >> courseNumber;
+			cout << endl;
+
+			studentList.printStudentsByCourse(courseNumber);
+			cout << endl;
+
+			break;
+						
+			// Print students on hold
+		case 5: 
+			studentList.printStudentsOnHold(tuitionRate);
+			cout << endl;
+
+			break;
+
+			// Incorrect selection
+		default: 
+			cout << "\nSorry. That is not a selection." << endl;
+
+			break;
+		}
+
+		system("Pause");
+		displayMenu();
+
+		cout << "\nEnter your choice: ";
+		cin >> choice;
+		cout << endl;
+	}
+}
